2392444276974cec6c963346056f57a6daf87f9b
Takahiro Ooishi <taka0125@gmail.com>
2023-12-08 22:40:13 +0900

    :+1: fix

--- lib/standalone_activerecord_boot_loader.rb
+++ lib/standalone_activerecord_boot_loader.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'standalone_activerecord_boot_loader/version'
+require 'erb'
 require 'zeitwerk'
 require 'active_record'
 require 'active_record/tasks/database_tasks'
@@ -25,31 +26,31 @@ module StandaloneActiverecordBootLoader
     private
 
     def setup_zeitwerk
-      loader = Zeitwerk::Loader.new
+      loader = ::Zeitwerk::Loader.new
       loader.push_dir(@app_root.join('app/models'))
       loader.setup
     end
 
     def setup_active_record
       create_database if @create_database
-      Time.zone = @timezone
-      ActiveRecord::Base.establish_connection(database_config[@env])
-      ActiveRecord::Base.time_zone_aware_attributes = true
+      ::Time.zone = @timezone
+      ::ActiveRecord::Base.establish_connection(database_config[@env])
+      ::ActiveRecord::Base.time_zone_aware_attributes = true
     end
 
     def create_database
-      tasks = ActiveRecord::Tasks::MySQLDatabaseTasks.new(build_database_config)
+      tasks = ::ActiveRecord::Tasks::MySQLDatabaseTasks.new(build_database_config)
       begin
         tasks.create
       rescue StandardError => e
         # ActiveRecord 5系
-        if defined?(ActiveRecord::Tasks::DatabaseAlreadyExists)
-          return if e.instance_of?(ActiveRecord::Tasks::DatabaseAlreadyExists)
+        if defined?(::ActiveRecord::Tasks::DatabaseAlreadyExists)
+          return if e.instance_of?(::ActiveRecord::Tasks::DatabaseAlreadyExists)
         end
 
         # ActiveRecord 6系以降
-        if defined?(ActiveRecord::DatabaseAlreadyExists)
-          return if e.instance_of?(ActiveRecord::DatabaseAlreadyExists)
+        if defined?(::ActiveRecord::DatabaseAlreadyExists)
+          return if e.instance_of?(::ActiveRecord::DatabaseAlreadyExists)
         end
 
         raise e
@@ -61,15 +62,15 @@ module StandaloneActiverecordBootLoader
     end
 
     def database_config
-      @database_config ||= YAML::load(ERB.new(File.read(database_yml_path)).result)
+      @database_config ||= ::YAML::load(::ERB.new(::File.read(database_yml_path)).result)
     end
 
     def build_database_config
-      unless defined?(ActiveRecord::DatabaseConfigurations::HashConfig)
+      unless defined?(::ActiveRecord::DatabaseConfigurations::HashConfig)
         return database_config[@env]
       end
 
-      config = ActiveRecord::DatabaseConfigurations::HashConfig.new(@env, 'primary', database_config[@env])
+      config = ::ActiveRecord::DatabaseConfigurations::HashConfig.new(@env, 'primary', database_config[@env])
       return config if config.respond_to?(:configuration_hash)
 
       database_config[@env]
